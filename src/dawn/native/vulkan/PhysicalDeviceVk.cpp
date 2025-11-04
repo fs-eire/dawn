f97f4a2c7013dcae133a453d3c1f163ff7ca9910
Yulong Wang <7679871+fs-eire@users.noreply.github.com>
2025-11-03 18:05:12 -0800

    [patch] dawn_force_enable_f16_nvidia_vulkan.patch
    - (private) Force enable f16 support for NVIDIA Vulkan
      Dawn disabled f16 support for NVIDIA Vulkan by default because of crashes in f16 CTS tests (crbug.com/tint/2164).
      Since the crashes are limited to specific GPU models, we patched Dawn to remove the restriction.

--- src/dawn/native/vulkan/PhysicalDeviceVk.cpp
+++ src/dawn/native/vulkan/PhysicalDeviceVk.cpp
@@ -345,11 +345,9 @@ void PhysicalDevice::InitializeSupportedFeaturesImpl() {
         mDeviceInfo.shaderFloat16Int8Features.shaderFloat16 == VK_TRUE &&
         mDeviceInfo._16BitStorageFeatures.storageBuffer16BitAccess == VK_TRUE &&
         mDeviceInfo._16BitStorageFeatures.uniformAndStorageBuffer16BitAccess == VK_TRUE) {
-        // TODO(crbug.com/tint/2164): Investigate crashes in f16 CTS tests to enable on NVIDIA.
-        if (!gpu_info::IsNvidia(GetVendorId())) {
-            EnableFeature(Feature::ShaderF16);
-            shaderF16Enabled = true;
-        }
+        // ONNX Runtime Patch: enable shaderF16 on all devices.
+        EnableFeature(Feature::ShaderF16);
+        shaderF16Enabled = true;
     }
 
     if (mDeviceInfo.HasExt(DeviceExt::DrawIndirectCount) &&
